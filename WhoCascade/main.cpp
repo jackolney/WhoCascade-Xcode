a22d01ed7c5c4f5beb53cedd197a0682758be99b
Jack Olney <jack.olney11@imperial.ac.uk>
2016-01-15 20:39:48 +0000

    Initial commit.

--- WhoCascade/main.cpp
+++ WhoCascade/main.cpp
@@ -6,10 +6,33 @@
 //  Copyright © 2016 Jack Olney. All rights reserved.
 //
 
+#include <iomanip>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
-    return 0;
+typedef double F(double,double);
+
+/*
+ Approximates y(t) in y'(t)=f(t,y) with y(a)=y0 and
+ t=a..b and the step size h.
+ */
+void euler(F f, double y0, double a, double b, double h)
+{
+	double y = y0;
+	for (double t = a; t < b; t += h)
+	{
+		std::cout << std::fixed << std::setprecision(3) << t << " " << y << "\n";
+		y += h * f(t, y);
+	}
+	std::cout << "done\n";
+}
+
+// Example: Newton's cooling law
+double newtonCoolingLaw(double, double t)
+{
+	return -0.07 * (t - 20);
+}
+
+int main()
+{
+	euler(newtonCoolingLaw, 100, 0, 100,  1);
 }
