657b11170f1ba47813dd78b7f2c6c364a896d354
Jack Olney <jack.olney11@imperial.ac.uk>
2016-01-15 21:06:49 +0000

    Fiddling

--- WhoCascade/main.cpp
+++ WhoCascade/main.cpp
@@ -9,30 +9,29 @@
 #include <iomanip>
 #include <iostream>
 
-typedef double F(double,double);
+using namespace std;
 
-/*
- Approximates y(t) in y'(t)=f(t,y) with y(a)=y0 and
- t=a..b and the step size h.
- */
-void euler(F f, double y0, double a, double b, double h)
+typedef double theFunc(double, double); // This a type called 'F' - which is a function taking two doubles
+
+/* Approximates y(t) in y'(t)=f(t,y) with y(a)=y0 and t=a..b and the step size h. */
+
+void Euler(theFunc f, double y0, double start, double stop, double step)
 {
 	double y = y0;
-	for (double t = a; t < b; t += h)
-	{
-		std::cout << std::fixed << std::setprecision(3) << t << " " << y << "\n";
-		y += h * f(t, y);
+	for (double t = start; t < stop; t += step) {
+		cout << fixed << setprecision(3) << t << " " << y << endl;
+		y += step * f(t, y);
 	}
-	std::cout << "done\n";
+	cout << "Finished." << endl;
 }
 
 // Example: Newton's cooling law
-double newtonCoolingLaw(double, double t)
+double NewtonCoolingLaw(double t, double y) // The second arguement here is the THING was are monitoring.
 {
-	return -0.07 * (t - 20);
+	return -0.07 * y;
 }
 
-int main()
-{
-	euler(newtonCoolingLaw, 100, 0, 100,  1);
+int main() {
+	Euler(NewtonCoolingLaw, 1000, 0, 20,  1);
+	return 0;
 }
