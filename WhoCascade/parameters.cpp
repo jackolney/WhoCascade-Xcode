997f0249655e06511b5cf9c353283723e5f5f3e3
Jack Olney <jack.olney11@imperial.ac.uk>
2016-01-17 13:06:22 +0000

    parameter header

---
+++ WhoCascade/parameters.cpp
@@ -0,0 +1,15 @@
+//
+//  parameters.cpp
+//  WhoCascade
+//
+//  Created by Jack Olney on 17/01/2016.
+//  Copyright © 2016 Jack Olney. All rights reserved.
+//
+
+#include "parameters.h"
+
+Params::Params() :
+mu(0.01)
+{}
+
+Params::~Params() {}
---
+++ WhoCascade/parameters.h
@@ -0,0 +1,26 @@
+//
+//  parameters.h
+//  WhoCascade
+//
+//  Created by Jack Olney on 17/01/2016.
+//  Copyright © 2016 Jack Olney. All rights reserved.
+//
+
+#ifndef parameters_h
+#define parameters_h
+
+#include <stdio.h>
+
+class Params {
+public:
+	Params(); // constructor
+	~Params(); // destructor
+	
+	/* stuff */
+	double mu;
+	
+private:
+	
+};
+
+#endif /* parameters_h */
